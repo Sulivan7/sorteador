409c8b63fbc7cabd433f3f42d8720b5b34bc93bf
sulivan7 <devsulivan@gmail.com>
2025-09-28 08:31:37 -0300

    fix: corrigir erro de número inválido

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("form-sorteador").addEventListener("submit", function(evento){
         evento.preventDefault();
-        let numeroMaximo = document.getElementById('numero-maximo').value;
-        numeroMaximo = parseInt(numeroMaximo);
+        let numeroMaximo = parseInt(document.getElementById('numero-maximo').value);
+            if (isNaN(numeroMaximo) || numeroMaximo < 2) {
+            alert("Digite um número maior ou igual a 2!");
+        return;
+}
 
         let numeroAleatorio = Math.random() * numeroMaximo;
         numeroAleatorio = Math.floor(numeroAleatorio + 1);
-       
+
         document.getElementById("resultado-valor").innerText = numeroAleatorio;
         document.querySelector(".resultado").style.display = "block";
     })
